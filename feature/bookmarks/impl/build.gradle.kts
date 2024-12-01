6489223971cc9e7803e02f7066d708b92004939f
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-12-01 12:47:06 +0000

    Move existing `:bookmarks` to `:bookmarks:impl`

--- feature/bookmarks/impl/build.gradle.kts
+++ feature/bookmarks/impl/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ android {
 
 dependencies {
     implementation(projects.core.data)
+    implementation(projects.feature.bookmarks.api)
 
     testImplementation(projects.core.testing)
 
