bc8fdcc16aaaac554463555b0294d8fddacf0909
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-12-01 12:32:22 +0000

    Create empty `:bookmarks:api` module

---
+++ feature/bookmarks/api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.feature)
+//    alias(libs.plugins.nowinandroid.android.library.compose)
+}
+
+android {
+    namespace = "com.google.samples.apps.nowinandroid.feature.bookmarks.api"
+}
+
+dependencies {
+//    implementation(projects.core.data)
+}
